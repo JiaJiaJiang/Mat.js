7c3361b1f8d3d3e6e1f3f7e080101b59c837b7c5
luojia <luojia@luojia.me>
2017-03-18 13:40:35 +0800

    no message

--- Mat.js
+++ Mat.js
@@ -211,10 +211,10 @@ function createClass(Constructor){
 			return createClass(Constructor);
 		}
 	}
-	const testArray=new Constructor(1);
+	var testArray=new Constructor(1);
 	Object.defineProperty(Matrix,'_instanceofTypedArray',{value:!!(TypedArray&&TypedArray.isPrototypeOf(testArray))});
 	testArray=null;
-	
+
 	Object.setPrototypeOf(Matrix,Constructor.prototype);
 	function Mat(l,c,fill){
 		const M=new Constructor(l*c);
